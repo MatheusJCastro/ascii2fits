fix undesired unit conversion

fits2ascii.py
@@ -11,10 +11,11 @@ def open_spec(fl_name):
     spec_data = hdul[0].data  # get the data
     spec_header = hdul[0].header  # get the header
 
-    if len(np.shape(spec_data)) == 2:
-        spec_data = spec_data[1] * 10**-17 * u.Unit('erg cm-2 s-1 AA-1')
-    else:
-        spec_data = spec_data * 10**-17 * u.Unit('erg cm-2 s-1 AA-1')
+    # Convert units, if needed
+    # if len(np.shape(spec_data)) == 2:
+    #     spec_data = spec_data[1] * 10**-17 * u.Unit('erg cm-2 s-1 AA-1')
+    # else:
+    #     spec_data = spec_data * 10**-17 * u.Unit('erg cm-2 s-1 AA-1')
 
     # Get the wavelength information from the header
     # CDELT1 or CD1_1
@@ -26,7 +27,7 @@ def open_spec(fl_name):
     return pd.DataFrame(data={"wl": wl, "data": spec_data})
 
 
-fl = "spec_name.fits"
+fl = "bulge6-bri_u3svrg2.fits"
 spec = open_spec(fl)
 
 spec.to_csv(fl[:-4] + ".csv", sep=",", header=False, index=False)